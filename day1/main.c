7dae6044feca4171f8b496f9c86862538e9a1fb6
xuecailing <xuecailing@xuecailingdeMacBook-Pro.local>
2015-07-27 23:07:56 +0800

    Initial Commit

---
+++ day1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  day1
+//
+//  Created by xuecailing on 15/7/27.
+//  Copyright (c) 2015年 xuecailing. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
